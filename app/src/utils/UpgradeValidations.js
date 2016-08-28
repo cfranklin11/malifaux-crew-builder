1df75730e56f9d2b4462dee03c1e79eb80ca771b
Craig Franklin <craigjfranklin@gmail.com>
2016-08-29 09:43:53 +1000

    create upgrade validation module

---
+++ app/src/utils/UpgradeValidations.js
@@ -0,0 +1,60 @@
+import * as regExps from '../constants/RegExps';
+
+function isCorrectFaction(faction, selectedFaction) {
+  const factionRegExp = new RegExp(selectedFaction.replace(/\s/g, '-'), 'i');
+  return factionRegExp.test(faction.replace(/\s/g, '-'));
+}
+
+function isCorrectName(nameRestrictions, characterName) {
+  if (nameRestrictions) {
+    const nameRegExp = new RegExp(nameRestrictions.replace(/,\s/g, '|'), 'i');
+    return nameRegExp.test(characterName);
+  }
+  return true;
+}
+
+function isExcludedCharacter(
+  restrictions1, restrictions2, character, leaderName) {
+  const nonRestrictionCapture1 =
+    regExps.NON_CAPTURE_REGEXP.exec(restrictions1) || ['', ''];
+  const nonRestrictionCapture2 =
+    regExps.NON_CAPTURE_REGEXP.exec(restrictions2) || ['', ''];
+  const nonRestriction1 = nonRestrictionCapture1[1];
+  const nonRestriction2 = nonRestrictionCapture2[1];
+  const separator = nonRestriction1 && nonRestriction2 ? '|' : '';
+  const nonRestrictions = `${nonRestriction1}${separator}${nonRestriction2}`;
+  const nonRestrictionsRegExp =
+    nonRestrictions ? new RegExp(nonRestrictions, 'i') : null;
+
+  if (nonRestrictionsRegExp) {
+    return nonRestrictionsRegExp
+      .test(character.station.concat(character.characteristics));
+  }
+  return regExps.LEADER_REGEXP.test(nonRestriction1.concat(nonRestriction2)) ?
+    character.name === leaderName : false;
+}
+
+function isCorrectLeader(
+  restrictions1, restrictions2, characterName, leaderName) {
+  return regExps.LEADER_REGEXP.test(restrictions1.concat(restrictions2)) &&
+    !regExps.NON_LEADER_REGEXP.test(restrictions1.concat(restrictions2)) ?
+    characterName === leaderName : true;
+}
+
+function meetsRestrictions(
+  restrictions1, restrictions2, station, characteristics) {
+  const restrictions1RegExp =
+    new RegExp(restrictions1
+      .replace(/,\s/g, '|')
+      .replace(regExps.NON_REGEXP, ''));
+  const restrictions2RegExp =
+    new RegExp(restrictions2
+      .replace(/,\s/g, '|')
+      .replace(regExps.NON_REGEXP, ''));
+  return restrictions1RegExp.test(station.concat(characteristics)) &&
+    restrictions2RegExp.test(station.concat(characteristics));
+}
+
+function isUpgradable(station) {
+  return regExps.UPGRADABLE_REGEXP.test(station);
+}
